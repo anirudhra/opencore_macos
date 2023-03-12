f9089c0be96ce4d57f1375aad3d18b9f660f10ac
Anirudh Acharya <anirudhra@users.noreply.github.com>
2023-03-11 22:14:30 -0800

    Add files via upload

---
+++ ryzen_patches/luminarAI.sh
@@ -0,0 +1,5 @@
+sudo perl -i -pe 's|\x90\x90\x90\x90\x56\xE8\x6A\x00|\x90\x90\x90\x90\x56\xE8\x3A\x00|sg' '/Applications/Luminar AI.app/Contents/Frameworks/MPCore.framework/Versions/A/Frameworks/libtbb.dylib'
+sudo perl -i -pe 's|\x90\x90\x90\x90\x56\xE8\x4A\x00|\x90\x90\x90\x90\x56\xE8\x1A\x00|sg' '/Applications/Luminar AI.app/Contents/Frameworks/MPCore.framework/Versions/A/Frameworks/libtbb.dylib'
+sudo perl -i -pe 's|\x90\x90\x90\x90\x56\xE8\x6A\x00|\x90\x90\x90\x90\x56\xE8\x3A\x00|sg' '/Applications/Luminar AI.app/Contents/Frameworks/MPCore.framework/Versions/A/Frameworks/libtbbmalloc.dylib'
+sudo perl -i -pe 's|\x90\x90\x90\x90\x56\xE8\x4A\x00|\x90\x90\x90\x90\x56\xE8\x1A\x00|sg' '/Applications/Luminar AI.app/Contents/Frameworks/MPCore.framework/Versions/A/Frameworks/libtbbmalloc.dylib'
+
